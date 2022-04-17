30d558ea6b28677005f87c9b2e2af04ca9a4b5e0
tekigeetanjali <96520682+tekigeetanjali@users.noreply.github.com>
2022-04-17 16:19:21 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
-import Weather from "./components/Weather"
-import "./styles.css";
-
-export default function App() {
-  return (
-    <div className="App">
-    <Weather/>
-    </div>
-  );
+import React,{useState} from 'react'
+import './styles.css'
+const App=()=>{
+  const [city,setCity]=useState('')
+  const [result,setResult]=useState('')
+  const changeHandler=e=>{
+    setCity(e.target.value);
+  }
+  const submitHandler=e=>{
+    e.preventDefault()
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=patiala&appid=df8805b218019cc28ee719f8c0d9667e').then(
+      response=>response.json()
+    ).then(data=>{
+      const kelvin=data.main.temp;
+      const celsius=kelvin -273.15;
+      console.log(celsius)
+      setResult("Temperature at"+" "+city+"\n"+Math.round(celsius)+"°C")
+    setCity("")
+    })
+  }
+  return(
+    <center>
+      <h1>Weather App</h1>
+      <form onSubmit={submitHandler}>
+     <input type="text" name="city" value={city} onChange={changeHandler}/><br/><br/>
+     <input type="submit" value="Get Temperature"/>
+      </form>
+      <h4>{result}</h4>
+      </center>
+  )
 }
+export default App
